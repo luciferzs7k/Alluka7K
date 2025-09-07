d85e3bc277dc4a68f8275c347662b5bac2edfd93
luciferzs7k <christianzika2k17@gmail.com>
2025-09-07 09:49:21 -0300

    primeiro commit

---
+++ alluka7k/core.py
@@ -0,0 +1,5 @@
+def start():
+    print("Alluka7k iniciada! Digite seu comando:")
+    # Aqui você pode adicionar mais funcionalidades para a aplicação
+    comando = input("> ")
+    print(f"Você digitou: {comando}")
---
+++ tests/test_core.py
@@ -0,0 +1,2 @@
+nome = input("Seu nome: ")
+print(f"Bem-vindo(a) ao projeto Alluka7k, {nome}! Vamos começar os testes.")
